35945f9ee846ca047e61da00d5c3279bf08c4ea6
iGotABadIdea <rv17314@gmail.com>
2021-08-23 23:58:27 +0530

    prime added

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,10 @@ import "./Home.css"
 function Home() {
     return (
         <div className="home">
-            <div className="home_container"></div>
+            <div className="home_container">
+                <img src="https://store-images.s-microsoft.com/image/apps.16285.14618985536919905.552c0017-6644-49a8-8467-8f7b34ce0428.30ad6b05-16d9-4d5e-a242-43107708a16a?mode=scale&q=90&h=1080&w=1920"alt=""/>
+            </div>
+
         </div>
     )
 }

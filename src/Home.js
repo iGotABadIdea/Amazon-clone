ee5fffa6070e4a9285b7ab2b71cffbc624c04b7e
iGotABadIdea <rv17314@gmail.com>
2021-10-06 23:55:01 +0530

    commited

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,9 @@ function Home() {
             <div className="home_container">
                 <img className="home_image" src="https://images-eu.ssl-images-amazon.com/images/G/02/UK-hq/2020/img/DVD/XCM_Manual_1200x445_1217661_1130674_IDx231217661_uk_dvd_digital_readiness_primevideo_banner_2_44c2b7ea_2f3b_4de2_ab69_9968a9e8112a_jpg_LOWER_QL85_.jpg"alt=""/>
             </div>
+            <div>
             <div className="home_row">
+            <Product/>
             <Product/>
             </div>
             <div className="home_row">
@@ -16,6 +18,7 @@ function Home() {
             </div>
             <div className="home_row">
                 
+            </div>
             </div>
 
         </div>

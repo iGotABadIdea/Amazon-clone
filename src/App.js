3e7d8d3c281797172960f0764a84a2105e741b3d
iGotABadIdea <rv17314@gmail.com>
2021-12-01 22:40:59 +0530

    Checkout route added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './Header'
+import Header from './Components/Header'
 import './App.css';
 import Home from "./Home";
 import Checkout from "./checkout";
--- src/checkout.jsx
+++ src/checkout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./checkout.css";
+import "./Components/checkout.css";
 
 function Checkout() {
     return (

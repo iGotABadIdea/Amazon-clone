b5fe3aba27c5911c55f6a2b2575df880584a45b9
iGotABadIdea <rv17314@gmail.com>
2021-12-03 14:41:23 +0530

    stucked

--- src/reducer.jsx
+++ src/reducer.jsx
@@ -2,7 +2,7 @@ export const initialState={
     cart:[],
 };
 const reducer = (state, action) => {
-    console.log(action);
+    console.log(action)
     switch (action.type) {
         case "ADD_TO_CART":
             return {

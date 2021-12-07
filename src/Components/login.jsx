071bef29fa2878bb6c82c18569afd765ed2fc51a
iGotABadIdea <rv17314@gmail.com>
2021-12-07 14:37:05 +0530

    user authentication added

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -1,26 +1,54 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React, {useState} from 'react'
+import { Link, useNavigate} from "react-router-dom"
+import { auth } from '../firebase'
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword,
+onAuthStateChanged, signOut} from "firebase/auth"
 import "./login.css"
 
 function Login() {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const signIn = async (e) =>{
+        e.preventDefault();
+        try {
+           const user = await signInWithEmailAndPassword(auth, email, password);
+           if (user) {
+               navigate("/");
+           }
+       } catch (error ) {
+           console.log(error.message);
+       }
+        
+    } 
+    const register = async (e) =>{
+        e.preventDefault();
+       try {
+           const user = await createUserWithEmailAndPassword(auth, email, password);
+           console.log(user);
+       } catch (error) {
+           alert(error.message)
+       }
+    }
     return (
         <div className="login">
             <Link to="/">
-             <img  className="loginLogo" src="https://wallpapercave.com/wp/wp7771200.png" alt=""/>
+             <img  className="loginLogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1000px-Amazon_logo.svg.png" alt=""/>
             </Link>
             <div className="loginContainer">
                 <h1>Sign-In</h1>
                 <form>
                     <h5>E-mail</h5>
-                    <input type="text"/>
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
                     <h5>Password</h5>
-                    <input type="password"/>
-                    <button className="loginSignInButton">Sign In</button>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                    <button  type='sumbit' onClick={signIn} className="loginSignInButton">Sign In</button>
                 </form>
                 <p>
                    By continuing, you agree to AMAZON CLONE Conditions of Use and Privacy Notice.
                 </p>
-                <button className="loginRegisterButton">Create your Amazon Account </button>
+                <button onClick={register} className="loginRegisterButton">Create your Amazon Account </button>
             </div>      
         </div>
     )
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,9 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import {initializeApp} from 'firebase/app'
+import {getAuth} from 'firebase/auth' 
+import {getFirestore} from 'firebase/firestore'
+
 const firebaseConfig = {
   apiKey: "AIzaSyBCh_yn9l70q2WPqZa3tcgh33PO46yL_wk",
   authDomain: "clone-163ee.firebaseapp.com",
@@ -8,3 +13,10 @@ const firebaseConfig = {
   appId: "1:626879109185:web:c7248529d307220ccc4ec0",
   measurementId: "G-4TE1G42M79"
 };
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+export { db, auth};
+

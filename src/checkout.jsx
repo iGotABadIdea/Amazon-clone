7f5080235eb81986743a3147ff8fe0fd7accd18d
iGotABadIdea <rv17314@gmail.com>
2021-12-11 13:12:57 +0530

    build created

--- src/checkout.jsx
+++ src/checkout.jsx
@@ -4,15 +4,18 @@ import "./Components/checkout.css";
 import Subtotal from './Components/subtotal';
 import CheckoutProduct from './Components/checkoutproduct';
 import { selectItems, selectTotal } from './slices/cartSlice';
+import { selectUser } from './slices/userSlice';
 
 function Checkout() {
     const items = useSelector(selectItems)
     const total = useSelector(selectTotal)
+    const user = useSelector(selectUser);  
     return (
         <div className="checkOut">
             <div className="checkoutLeft">
                 <img className="checkoutAd" src="https://m.media-amazon.com/images/G/01/AdProductsWebsite/images/AUX/ILB_BrightColors_Approved._TTW_.jpg" alt=""/>
                 <div >
+                    <h3> Hello, {user?.email}</h3>
                     <h1 className="checkoutTitle">
                         {items.length === 0? "Your Amazon Cart is empty": "Shopping Cart"}
                     </h1>
